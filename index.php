5dc86f66ab528671e0c9021b5bf55646ef2852b4
johnchiong <152459351+johnchiong@users.noreply.github.com>
2024-11-25 23:09:02 +0800

    Style

--- index.php
+++ index.php
@@ -89,6 +89,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Math Quiz</title>
+    <link rel="stylesheet" href="style.css">
 
     <script>
         document.addEventListener('contextmenu', event => event.preventDefault());
